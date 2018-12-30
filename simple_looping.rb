f01c41e7dfe3ce4a07ca170ad36f1e3cf620cbef
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-12-30 04:58:18 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,9 @@
 def loop_iterator(number_of_times)
   loop do
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  puts phrase
+  counter += 1
+  break if counter == number_of_times
 end
 
 def times_iterator(number_of_times)
