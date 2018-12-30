37bb51c91f4315707ffa4903ba70e811202e9d98
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-12-30 05:01:51 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   puts phrase
   counter += 1
-  break if counter < 7
+  break if counter >=7
  end
 end
 
