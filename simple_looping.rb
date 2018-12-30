ce1258f7d099e1c370c6ef1a4d708bec574a0f39
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-12-30 05:02:29 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,11 +1,8 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  loop do
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  puts phrase
-  counter += 1
-  break if counter >=7
+  
  end
 end
 
